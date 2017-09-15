2c1c95eb7fcb862e8109de5c5b43eb2bd789c4d9
Chris Dufour <cdufour@west.com>
2017-09-15 16:45:26 -0400

    Initial commit

---
+++ PivotalWebAppTest/DeployAll.cmd
@@ -0,0 +1,21 @@
+:: This needs to be run from the application root
+
+:: Scenario 1 - Self-Contained .NETCore2.0 deployment targeting Windows2012R2
+rmdir /s /q .\bin\publish-windows-sc
+dotnet publish -f netcoreapp2.0 -r win10-x64 -o %CD%\bin\publish-windows-sc --self-contained true
+cf push -f manifest-windows-sc.yml -p .\bin\publish-windows-sc
+
+:: Scenario 2 - Self-Contained .NETCore2.0 deployment targeting cflinuxfs2
+rmdir /s /q .\bin\publish-linux-sc
+dotnet publish -f netcoreapp2.0 -r ubuntu-x64 -o %CD%\bin\publish-linux-sc --self-contained true
+cf push -f manifest-linux-sc.yml -p .\bin\publish-linux-sc
+
+:: Scenario 3 - Framework-Dependent .NETCore2.0 deployment targeting Windows2012R2 
+rmdir /s /q .\bin\publish-windows-fd
+dotnet publish -f netcoreapp2.0 -r win10-x64 -o %CD%\bin\publish-windows-fd --self-contained false
+cf push -f manifest-windows-fd.yml -p .\bin\publish-windows-fd
+
+:: Scenario 4 - Framework-Dependent .NETCore2.0 deployment targeting cflinuxfs2
+rmdir /s /q .\bin\publish-linux-fd
+dotnet publish -f netcoreapp2.0 -r ubuntu-x64 -o %CD%\bin\publish-linux-fd --self-contained false
+cf push -f manifest-linux-fd.yml -p .\bin\publish-linux-fd
--- PivotalWebAppTest/Program.cs
+++ PivotalWebAppTest/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace PivotalWebAppTest
 {
@@ -19,7 +14,27 @@ namespace PivotalWebAppTest
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+            .UseUrls(GetServerUrls(args))    
+            .UseStartup<Startup>()
                 .Build();
+
+        /// <summary>
+        /// Update the server URLs from the command line. 
+        /// Required when starting a Pivotal container
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static string[] GetServerUrls(IReadOnlyList<string> args)
+        {
+            var urls = new List<string>();
+            for (var i = 0; i < args.Count; i++)
+            {
+                if ("--server.urls".Equals(args[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    urls.Add(args[i + 1]);
+                }
+            }
+            return urls.ToArray();
+        }
     }
 }
